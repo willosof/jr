84f1b9749cc97c1fe2992fa76246e0f1dd3112bb
William Viker <william@trippelm.no>
2018-03-11 16:20:39 +0100

    Debug

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
- var express = require('express');
+process.env['DEBUG'] = '*,-config,-engine*,-socket.io*,-express*';
+
+var express = require('express');
 var app     = express();
 var server  = require('http').createServer(app);
 var io      = require('socket.io')(server);
