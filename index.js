02bea3186a5fd1f76262fa1865c7a265d9b77a81
Garder94 <tgarder@hotmail.com>
2018-03-21 15:13:57 +0100

    added heading

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ system.on('position_calculate', function() {
 					lat: mob_position[0],
 					lon: mob_position[1]
 			});
-			console.log("MOB lon: " + mob_position[0]);
+			console.log("Kursen er =" + heading);
 				system.emit('distance', parseInt(dist));
 				//console.log("CURRENT DISTANCE FROM JET " + parseInt(dist) );
 			}
